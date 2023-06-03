882b140c4718abfce1746684c5c2cb53a7ee98b0
rldnrl <devpark1002@gmail.com>
2023-06-03 17:52:13 +0900

    feat: implement sort

---
+++ src/utils/sort.ts
@@ -0,0 +1,10 @@
+export const sort = <T>(
+  array: readonly T[],
+  getter: (item: T) => number,
+  desc = false
+) => {
+  if (!array) return [];
+  const asc = (a: T, b: T) => getter(a) - getter(b);
+  const dsc = (a: T, b: T) => getter(b) - getter(a);
+  return array.slice().sort(desc === true ? dsc : asc);
+};
