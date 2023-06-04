97b9d3a427f8cba5b89cf4cf00924a81cbb5074c
rldnrl <devpark1002@gmail.com>
2023-06-04 15:05:19 +0900

    feat: implement icons

---
+++ src/icons/moon.tsx
@@ -0,0 +1,18 @@
+type MoonProps = {
+  className?: string;
+};
+
+export const Moon = ({ className }: MoonProps) => {
+  return (
+    <svg
+      viewBox="0 0 20 20"
+      xmlns="http://www.w3.org/2000/svg"
+      aria-hidden="true"
+      className={className}>
+      <path
+        clipRule="evenodd"
+        fillRule="evenodd"
+        d="M7.455 2.004a.75.75 0 01.26.77 7 7 0 009.958 7.967.75.75 0 011.067.853A8.5 8.5 0 116.647 1.921a.75.75 0 01.808.083z"></path>
+    </svg>
+  );
+};
