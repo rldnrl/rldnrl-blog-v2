86e2d5c5a077aea33b8bf4df05011cd11ff2d132
rldnrl <devpark1002@gmail.com>
2023-06-04 15:06:02 +0900

    feat: implement grid container

---
+++ src/components/grid-container.tsx
@@ -0,0 +1,19 @@
+import { cn } from "@/utils/cn";
+import { ReactNode } from "react";
+
+type GridContainerProps = {
+  className?: string;
+  children?: ReactNode;
+};
+
+export const GridContainer = ({ className, children }: GridContainerProps) => {
+  return (
+    <div
+      className={cn(
+        `grid grid-cols-12 gap-0 md:gap-x-8 lg:gap-x-16`,
+        className
+      )}>
+      {children}
+    </div>
+  );
+};
