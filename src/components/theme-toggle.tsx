f64cb4cf0bdfe2229d69e9858094a94e1c4f0e3a
rldnrl <devpark1002@gmail.com>
2023-06-05 15:09:55 +0900

    feat: add aria-label

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -9,21 +9,20 @@ export const ThemeToggle = () => {
   const [mounted, setMounted] = useState(false);
   const { theme, setTheme, resolvedTheme } = useTheme();
 
-  console.log({ theme });
-
   useEffect(() => setMounted(true), []);
+
+  const isDarkMode = theme === "dark" || resolvedTheme === "dark";
 
   return (
     <button
       className="text-gray-500 ml-2 hover:bg-slate-100 focus:outline-none focus:ring-4 focus:ring-gray-200  rounded-lg text-sm p-2 dark:text-white dark:focus:ring-gray-700 dark:hover:bg-gray-700"
       id="theme-toggle"
       type="button"
-      onClick={() =>
-        setTheme(
-          theme === "dark" || resolvedTheme === "dark" ? "light" : "dark"
-        )
-      }>
-      {mounted && (theme === "dark" || resolvedTheme === "dark") ? (
+      aria-label={
+        mounted && isDarkMode ? "Theme Light Mode" : "Theme Dark Mode"
+      }
+      onClick={() => setTheme(isDarkMode ? "light" : "dark")}>
+      {mounted && isDarkMode ? (
         <LightIcon className="w-6 h-6 " />
       ) : (
         <MoonIcon className="w-6 h-6" />
