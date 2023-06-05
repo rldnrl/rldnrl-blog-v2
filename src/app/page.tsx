03a09d9209170c53a58d0252448f9177f2c4eb64
rldnrl <devpark1002@gmail.com>
2023-06-05 22:23:04 +0900

    feat: apply articles component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
+import { Metadata } from "next";
+import Link from "next/link";
+import { Articles } from "@/components/blog/articles";
 import { GridContainer } from "@/components/grid-container";
 import { siteMetadata } from "@/constant/site-metadata";
 import { PostService } from "@/service/posts.service";
-import { Post } from "@/types/post";
-import { Metadata } from "next";
-import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Welcome to Rldnrl Blog",
@@ -25,42 +25,7 @@ export default async function Home() {
           </p>
         </div>
         <hr className="safe-paddings col-span-full md:col-start-2 md:col-end-12 h-px mb-8 bg-gray-200 border-0 dark:bg-gray-700" />
-        <ol className="safe-paddings col-span-full dark:text-slate-200 md:col-start-2 md:col-end-12 md:relative md:border-l md:border-gray-200 md:dark:border-gray-700">
-          {posts.map((post: Post) => {
-            if (post.draft && process.env.NODE_ENV !== "development")
-              return null;
-
-            return (
-              <li key={post.id} className="md:pl-8">
-                <div className="hidden absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700 md:block" />
-                <time className="text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-                  {post.date.toLocaleDateString("ko-kr", {
-                    year: "numeric",
-                    month: "short",
-                    day: "numeric",
-                    timeZone: "UTC",
-                  })}
-                </time>
-                <article className="mt-2">
-                  <Link
-                    className="ease-in duration-200 hover:text-[#00E599]"
-                    href={`/blog/${post.slug}`}>
-                    <h1 className="text-2xl font-semibold leading-tight md:text-3xl">
-                      {post.title}
-                    </h1>
-                  </Link>
-                  <p className="mt-4 !leading-normal lg:mt-5">{post.summary}</p>
-                  <Link
-                    className="inline-flex !leading-none items-center relative transition-colors duration-500 before:absolute before:-bottom-1.5 before:left-0 before:h-1.5 before:w-full before:transition-all before:duration-500 hover:before:bottom-full hover:before:opacity-0 before:pointer-events-none before:bg-[#31C48D] hover:text-[#31C48D] dark:before:bg-[#00E599] dark:text-white dark:hover:text-[#00E599] mt-4 font-semibold lg:mt-5"
-                    href={`/blog/${post.slug}`}>
-                    Read more
-                  </Link>
-                </article>
-                <div className="safe-paddings col-span-full md:col-start-2 md:col-end-12 my-16 bg-gray-200 border-0 dark:bg-gray-700" />
-              </li>
-            );
-          })}
-        </ol>
+        <Articles posts={posts} />
         <div className="safe-paddings grid justify-end col-span-full mt-16 md:col-start-10 md:col-end-12">
           <Link
             className="text-green-300 hover:text-[#00E599] cursor-pointer"
--- src/components/blog/articles-view.tsx
+++ src/components/blog/articles-view.tsx
@@ -5,8 +5,7 @@ import { Tags } from "../tags";
 import { useSearchParams } from "next/navigation";
 import { sort } from "@/utils/sort";
 import { Pagination } from "../pagination";
-import { EnvService } from "@/service/env.service";
-import Link from "next/link";
+import { Articles } from "./articles";
 
 const POSTS_PER_PAGE = 3;
 
@@ -39,43 +38,7 @@ export const ArticlesView = ({ tags, posts }: ArticlesViewProps) => {
   return (
     <>
       <Tags tags={tags} currentTag={currentTag as string} />
-      <ol className="safe-paddings col-span-full dark:text-slate-200 md:col-start-2 md:col-end-12 md:relative md:border-l md:border-gray-200 md:dark:border-gray-700">
-        {filteredPostsByTag.map((post: Post) => {
-          if (post.draft && EnvService.isProduction()) return null;
-
-          const formattedDate = post.date.toLocaleDateString("ko-kr", {
-            year: "numeric",
-            month: "short",
-            day: "numeric",
-            timeZone: "UTC",
-          });
-
-          return (
-            <li key={post.id} className="md:pl-8">
-              <div className="hidden absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700 md:block" />
-              <time className="text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-                {formattedDate}
-              </time>
-              <article className="mt-2">
-                <Link
-                  className="ease-in duration-200 hover:text-green-400"
-                  href={`/blog/${post.slug}`}>
-                  <h1 className="text-2xl font-semibold leading-tight md:text-3xl">
-                    {post.title}
-                  </h1>
-                </Link>
-                <p className="mt-4 lg:mt-5">{post.summary}</p>
-                <Link
-                  className="inline-flex items-center relative transition-colors duration-500 before:absolute before:-bottom-1.5 before:left-0 before:h-1.5 before:w-full before:transition-all before:duration-500 hover:before:bottom-full hover:before:opacity-0 before:pointer-events-none before:bg-green-300 hover:text-green-400 dark:before:bg-green-400 dark:text-white dark:hover:text-green-400 mt-4 font-semibold lg:mt-5"
-                  href={`/blog/${post.slug}`}>
-                  Read more
-                </Link>
-              </article>
-              <div className="safe-paddings col-span-full md:col-start-2 md:col-end-12 my-16 bg-gray-200 border-0 dark:bg-gray-700" />
-            </li>
-          );
-        })}
-      </ol>
+      <Articles posts={filteredPostsByTag} />
       {currentTag === "all" && totalPages > 1 && (
         <Pagination currentPage={currentPage} totalPages={totalPages} />
       )}
