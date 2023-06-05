4739c5ceb0c819189fad577c243abf56258e1d5f
rldnrl <devpark1002@gmail.com>
2023-06-05 14:01:19 +0900

    feat: implement pagination

--- src/components/blog/articles-view.tsx
+++ src/components/blog/articles-view.tsx
@@ -4,6 +4,9 @@ import { Post } from "@/types/post";
 import { Tags } from "../tags";
 import { useSearchParams } from "next/navigation";
 import { sort } from "@/utils/sort";
+import { Pagination } from "../pagination";
+
+const POSTS_PER_PAGE = 3;
 
 type ArticlesViewProps = {
   tags: string[];
@@ -14,13 +17,22 @@ export const ArticlesView = ({ tags, posts }: ArticlesViewProps) => {
   const searchParams = useSearchParams();
 
   const currentTag = searchParams.get("tag") ?? ("all" as string);
+  const currentPage = parseInt(searchParams.get("page") ?? "1");
+  const totalPages = Math.ceil(posts.length / POSTS_PER_PAGE);
+
   const sortedAllPosts = sort(posts, (post: Post) => post.date.getTime(), true);
   const filteredPostsByTag =
     currentTag === "all"
-      ? [...sortedAllPosts]
-      : sortedAllPosts.filter((post) =>
-          post.tags?.includes(currentTag as string)
-        );
+      ? [...sortedAllPosts].slice(
+          POSTS_PER_PAGE * (currentPage - 1),
+          POSTS_PER_PAGE * currentPage
+        )
+      : sortedAllPosts
+          .filter((post) => post.tags?.includes(currentTag as string))
+          .slice(
+            POSTS_PER_PAGE * (currentPage - 1),
+            POSTS_PER_PAGE * currentPage
+          );
 
   return (
     <>
@@ -60,6 +72,9 @@ export const ArticlesView = ({ tags, posts }: ArticlesViewProps) => {
           );
         })}
       </ol>
+      {currentTag === "all" && totalPages > 1 && (
+        <Pagination currentPage={currentPage} totalPages={totalPages} />
+      )}
     </>
   );
 };
