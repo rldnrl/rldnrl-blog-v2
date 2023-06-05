47716dac9e6e7be72346100cdaff7e90e18732a0
rldnrl <devpark1002@gmail.com>
2023-06-05 23:27:07 +0900

    feat: delete is-dark-mode variable

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -2,30 +2,26 @@
 
 import { Moon as MoonIcon } from "@/icons/moon";
 import { Light as LightIcon } from "@/icons/light";
-import { useEffect, useState } from "react";
 import { useTheme } from "next-themes";
 
 export const ThemeToggle = () => {
-  const [mounted, setMounted] = useState(false);
-  const { theme, setTheme, resolvedTheme } = useTheme();
+  const { theme, setTheme } = useTheme();
 
-  useEffect(() => setMounted(true), []);
-
-  const isDarkMode = theme === "dark" || resolvedTheme === "dark";
+  const handleClick = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
 
   return (
     <button
       className="text-gray-500 ml-2 hover:bg-slate-100 focus:outline-none focus:ring-4 focus:ring-gray-200  rounded-lg text-sm p-2 dark:text-white dark:focus:ring-gray-700 dark:hover:bg-gray-700"
       id="theme-toggle"
       type="button"
-      aria-label={
-        mounted && isDarkMode ? "Theme Light Mode" : "Theme Dark Mode"
-      }
-      onClick={() => setTheme(isDarkMode ? "light" : "dark")}>
-      {mounted && isDarkMode ? (
-        <LightIcon className="w-6 h-6 " />
-      ) : (
+      aria-label={theme === "dark" ? "Theme Light Mode" : "Theme Dark Mode"}
+      onClick={handleClick}>
+      {theme === "dark" ? (
         <MoonIcon className="w-6 h-6" />
+      ) : (
+        <LightIcon className="w-6 h-6 " />
       )}
     </button>
   );
