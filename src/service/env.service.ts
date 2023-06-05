bfac5cd77c8f698f827bdda505e8ae216f79d496
rldnrl <devpark1002@gmail.com>
2023-06-05 22:19:28 +0900

    feat: add is production method

--- src/service/env.service.ts
+++ src/service/env.service.ts
@@ -2,4 +2,7 @@ export class EnvService {
   public static isDevelopment(): boolean {
     return process.env.NODE_ENV === "development";
   }
+  public static isProduction(): boolean {
+    return process.env.NODE_ENV === "production";
+  }
 }
