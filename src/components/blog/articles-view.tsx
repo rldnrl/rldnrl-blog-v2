620618c9133832e6c86857327ce5db2bcaeefef8
rldnrl <devpark1002@gmail.com>
2023-06-05 22:18:47 +0900

    refactor: refactor formatted date

--- src/components/blog/articles-view.tsx
+++ src/components/blog/articles-view.tsx
@@ -41,18 +41,20 @@ export const ArticlesView = ({ tags, posts }: ArticlesViewProps) => {
       <Tags tags={tags} currentTag={currentTag as string} />
       <ol className="safe-paddings col-span-full dark:text-slate-200 md:col-start-2 md:col-end-12 md:relative md:border-l md:border-gray-200 md:dark:border-gray-700">
         {filteredPostsByTag.map((post: Post) => {
-          if (post.draft && !EnvService.isDevelopment()) return null;
+          if (post.draft && process.env.NODE_ENV !== "development") return null;
+
+          const formattedDate = post.date.toLocaleDateString("ko-kr", {
+            year: "numeric",
+            month: "short",
+            day: "numeric",
+            timeZone: "UTC",
+          });
 
           return (
             <li key={post.id} className="md:pl-8">
               <div className="hidden absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700 md:block" />
               <time className="text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-                {post.date.toLocaleDateString("ko-kr", {
-                  year: "numeric",
-                  month: "short",
-                  day: "numeric",
-                  timeZone: "UTC",
-                })}
+                {formattedDate}
               </time>
               <article className="mt-2">
                 <Link
@@ -62,9 +64,9 @@ export const ArticlesView = ({ tags, posts }: ArticlesViewProps) => {
                     {post.title}
                   </h1>
                 </Link>
-                <p className="mt-4 !leading-normal lg:mt-5">{post.summary}</p>
+                <p className="mt-4 lg:mt-5">{post.summary}</p>
                 <Link
-                  className="inline-flex !leading-none items-center relative transition-colors duration-500 before:absolute before:-bottom-1.5 before:left-0 before:h-1.5 before:w-full before:transition-all before:duration-500 hover:before:bottom-full hover:before:opacity-0 before:pointer-events-none before:bg-green-300 hover:text-green-400 dark:before:bg-green-400 dark:text-white dark:hover:text-green-400 mt-4 font-semibold lg:mt-5"
+                  className="inline-flex items-center relative transition-colors duration-500 before:absolute before:-bottom-1.5 before:left-0 before:h-1.5 before:w-full before:transition-all before:duration-500 hover:before:bottom-full hover:before:opacity-0 before:pointer-events-none before:bg-green-300 hover:text-green-400 dark:before:bg-green-400 dark:text-white dark:hover:text-green-400 mt-4 font-semibold lg:mt-5"
                   href={`/blog/${post.slug}`}>
                   Read more
                 </Link>
