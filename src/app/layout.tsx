18e3343a740974222c62f213d7f17082bb7614ec
rldnrl <devpark1002@gmail.com>
2023-06-06 11:19:05 +0900

    feat: change language

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="ko">
       <body
         className={cn("relative flex min-h-[100vh] flex-col scrollbar-hide")}>
         <ThemeProvider>
