1bc188584c6b0707dd0dbf25b41dbd75597c5a68
rldnrl <devpark1002@gmail.com>
2023-06-06 10:56:01 +0900

    feat: implement sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,20 @@
+import { MetadataRoute } from "next";
+import { siteMetadata } from "@/constant/site-metadata";
+import { PostService } from "@/service/posts.service";
+import { Post } from "@/types/post";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const posts = await PostService.fetchPosts();
+
+  return [
+    {
+      url: siteMetadata.siteUrl,
+      lastModified: new Date(),
+    },
+    { url: `${siteMetadata.siteUrl}/about`, lastModified: new Date() },
+    ...posts.map((post: Post) => ({
+      url: `${siteMetadata.siteUrl}/${post.slug}`,
+      lastModified: `${siteMetadata.siteUrl}/${post.date}`,
+    })),
+  ];
+}
