94890ba30ba30db0d5839d72e4bb85a32ec19e55
rldnrl <devpark1002@gmail.com>
2023-06-06 20:41:23 +0900

    feat: add sitemap about tag, page

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,9 +2,17 @@ import { MetadataRoute } from "next";
 import { siteMetadata } from "@/constant/site-metadata";
 import { PostService } from "@/service/posts.service";
 import { Post } from "@/types/post";
+import { EnvService } from "@/service/env.service";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const posts = await PostService.fetchPosts();
+  const filteredPosts = posts.filter(
+    (post) => !(post.draft && EnvService.isProduction())
+  );
+  const tags = await PostService.getTags();
+  const totalPages = [...Array(Math.ceil(filteredPosts.length / 3)).keys()].map(
+    (i) => i + 1
+  );
 
   return [
     {
@@ -12,9 +20,17 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       lastModified: new Date(),
     },
     { url: `${siteMetadata.siteUrl}/about`, lastModified: new Date() },
-    ...posts.map((post: Post) => ({
+    ...filteredPosts.map((post: Post) => ({
       url: `${siteMetadata.siteUrl}/blog/${post.slug}`,
       lastModified: `${siteMetadata.siteUrl}/${post.date}`,
+    })),
+    ...tags.map((tag: string) => ({
+      url: `${siteMetadata.siteUrl}/blog/tag/${tag}`,
+      lastModified: new Date(),
+    })),
+    ...totalPages.map((page) => ({
+      url: `${siteMetadata.siteUrl}/blog/page/${page}`,
+      lastModified: new Date(),
     })),
   ];
 }
