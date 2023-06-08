05947f7c0deb6460e35ad982f0a0d3d93b1b92fd
rldnrl <devpark1002@gmail.com>
2023-06-08 21:21:56 +0900

    feat: apply use scroll center

--- src/components/tags/tags.tsx
+++ src/components/tags/tags.tsx
@@ -2,6 +2,8 @@
 
 import { useRouter } from "next/navigation";
 import { TagItem } from "./tag-item";
+import { useRef } from "react";
+import useScrollCenter from "@/hooks/use-scroll-center";
 
 type TagsProps = {
   tags: string[];
@@ -10,6 +12,7 @@ type TagsProps = {
 
 export const Tags = ({ tags, currentTag }: TagsProps) => {
   const router = useRouter();
+  const tagsRef = useRef<HTMLUListElement>(null);
 
   const handleClick = (tag: string) => {
     if (tag === "all") {
@@ -19,10 +22,14 @@ export const Tags = ({ tags, currentTag }: TagsProps) => {
     }
   };
 
+  useScrollCenter({ ref: tagsRef, targetId: "active" });
+
   return (
     <div className="col-span-12 md:col-start-2 md:col-end-12 mb-8">
       <div className="w-full justify-between space-y-4">
-        <ul className="flex space-x-1 overflow-auto whitespace-nowrap scrollbar-hide mask-fadeout-right">
+        <ul
+          className="flex space-x-1 overflow-auto whitespace-nowrap scrollbar-hide mask-fadeout-right"
+          ref={tagsRef}>
           {tags.map((tag) => (
             <TagItem
               key={tag}
