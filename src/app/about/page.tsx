dc5b45051c8271dcd19c88eb0673de37fd8a5806
rldnrl <devpark1002@gmail.com>
2023-06-08 22:01:19 +0900

    feat: add front matter

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -12,10 +12,11 @@ export default async function About() {
   return (
     <div className="safe-paddings pt-6 md:pt-12">
       <GridContainer className="relative mx-auto px-4 md:max-w-none md:px-8 lg:max-w-4xl">
-        <section className="safe-paddings col-span-full md:col-start-2 md:col-end-12 dark:text-slate-200">
-          <div className="col-span-full md:col-start-2 md:col-end-12 prose-md prose-headings:font-headings prose-h1:mb-8 prose-h2:mb-4 prose-h2:mt-0 prose-ul:mb-8 prose-headings:leading-tighter container prose prose-lg prose-headings:font-bold prose-headings:tracking-tighter prose-a:decoration-[#00e599] prose-img:rounded-md prose-img:shadow-lg dark:prose-invert dark:prose-headings:text-slate-200 dark:prose-a:text-primary-400 prose-code:before:content-none prose-code:after:content-none prose-pre:p-0">
-            {about?.content}
-          </div>
+        <section className="safe-paddings col-span-full md:col-start-2 md:col-end-12 dark:text-slate-200 prose dark:prose-invert">
+          <h1>{about.title}</h1>
+          {about.description && <p className="text-xl">{about.description}</p>}
+          <hr />
+          {about?.content}
         </section>
       </GridContainer>
     </div>
