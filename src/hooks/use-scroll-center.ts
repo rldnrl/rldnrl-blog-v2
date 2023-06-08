ae4fd926bc0d742ac42c154e36c1e82e97ced21c
rldnrl <devpark1002@gmail.com>
2023-06-08 21:21:26 +0900

    feat: implement use scroll center

---
+++ src/hooks/use-scroll-center.ts
@@ -0,0 +1,41 @@
+import { useLayoutEffect } from "react";
+
+interface UseScrollCenterProps {
+  ref: React.RefObject<HTMLUListElement>;
+  targetId: string;
+}
+
+const useScrollCenter = ({ ref, targetId }: UseScrollCenterProps) => {
+  useLayoutEffect(() => {
+    const tagsWrapElement = ref.current;
+
+    if (!tagsWrapElement) {
+      return;
+    }
+
+    const isScrollActivated =
+      tagsWrapElement.scrollWidth >= tagsWrapElement.offsetWidth;
+
+    if (!isScrollActivated) {
+      return;
+    }
+
+    const activeTagElement = tagsWrapElement.querySelector<HTMLUListElement>(
+      `#${targetId}`
+    );
+
+    if (!activeTagElement) {
+      return;
+    }
+
+    const offsetX = activeTagElement.offsetLeft - tagsWrapElement.offsetLeft;
+    tagsWrapElement.scrollTo(
+      offsetX -
+        tagsWrapElement.offsetWidth / 2 +
+        activeTagElement.offsetWidth / 2,
+      0
+    );
+  }, [ref, targetId]);
+};
+
+export default useScrollCenter;
