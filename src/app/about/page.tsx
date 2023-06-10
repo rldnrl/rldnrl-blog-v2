892662fa127a2affddac74c4b00f3e17393b42f7
rldnrl <devpark1002@gmail.com>
2023-06-10 12:26:31 +0900

    feat: add nullable operator

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -34,7 +34,7 @@ export default async function About() {
     <div className="safe-paddings pt-6 md:pt-12">
       <GridContainer className="relative mx-auto px-4 md:max-w-none md:px-8 lg:max-w-4xl">
         <section className="safe-paddings col-span-full md:col-start-2 md:col-end-12 dark:text-slate-200 prose dark:prose-invert">
-          <h1>{about.title}</h1>
+          <h1>{about?.title ?? About}</h1>
           {about.description && <p className="text-xl">{about.description}</p>}
           <hr />
           {about?.content}
