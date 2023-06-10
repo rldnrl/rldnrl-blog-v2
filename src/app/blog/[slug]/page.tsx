0c7a2ed0facbbf61fa0660578a655762f582ebad
rldnrl <devpark1002@gmail.com>
2023-06-10 15:06:33 +0900

    chore: remove style

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -80,7 +80,7 @@ export default async function Blog({ params }: BlogProps) {
         <div className="col-span-full md:col-start-2 md:col-end-12 prose-md prose-headings:font-headings prose-headings:leading-tighter container prose prose-lg prose-headings:font-bold prose-headings:tracking-tighter prose-a:decoration-primary-1 prose-img:rounded-md prose-img:shadow-lg dark:prose-invert dark:prose-headings:text-slate-200 dark:prose-a:text-primary-400 prose-code:before:content-none prose-code:after:content-none prose-pre:p-0">
           {content}
         </div>
-        <div className="col-span-full md:col-start-2 md:col-end-12 prose-md prose-headings:font-headings prose-headings:leading-tighter container prose prose-lg prose-headings:font-bold prose-headings:tracking-tighter prose-a:decoration-primary-1 prose-img:rounded-md prose-img:shadow-lg dark:prose-invert dark:prose-headings:text-slate-200 dark:prose-a:text-primary-400 prose-code:before:content-none prose-code:after:content-none prose-pre:p-0">
+        <div>
           <Comments frontMatter={frontMatter} />
         </div>
         <div className="safe-paddings col-span-full md:col-start-2 md:col-end-12 mt-16">
