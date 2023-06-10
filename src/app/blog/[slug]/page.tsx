f7460d76494404c9a0753178aee0871df3317b10
rldnrl <devpark1002@gmail.com>
2023-06-10 13:05:57 +0900

    feat: add description in metadata

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -41,6 +41,7 @@ export async function generateMetadata({
 
   return {
     title: post.title,
+    description: post.summary,
     openGraph: {
       title: post.title,
       description: post.summary,
