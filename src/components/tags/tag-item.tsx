1e97d0c484e1df8483829604513f29351ae39725
rldnrl <devpark1002@gmail.com>
2023-06-17 12:03:52 +0900

    feat: change text color in dark mode

--- src/components/tags/tag-item.tsx
+++ src/components/tags/tag-item.tsx
@@ -19,7 +19,7 @@ export const TagItem = ({ checked, tag }: TagItemProps) => {
         {
           "bg-[#F5F5F5] text-[#333333] border-[#CCCCCC] dark:bg-[#343434] dark:text-[#ededed] dark:border-[#505050]":
             checked,
-          "bg-white border-gray-300 hover:border-gray-400 text-gray-800 hover:text-[#707070] dark:bg-[#1c1c1c] dark:border-[#3e3e3e] dark:hover:border-[#707070] dark:text-[#707070] dark:hover:text-[#ededed]":
+          "bg-white border-gray-300 hover:border-gray-400 text-gray-800 hover:text-[#707070] dark:bg-[#1c1c1c] dark:border-[#3e3e3e] dark:hover:border-[#707070] dark:text-[#979797] dark:hover:text-[#ededed]":
             !checked,
         }
       )}
