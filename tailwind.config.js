00b67e77bf61df5d0fae453e1850924cfb08da4e
rldnrl <devpark1002@gmail.com>
2023-06-29 22:30:49 +0900

    feat: remove container

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,13 +8,6 @@ module.exports = {
     './src/**/*.{ts,tsx}',
 	],
   theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
-    },
     zIndex: {
       'nav': '10'
     },
