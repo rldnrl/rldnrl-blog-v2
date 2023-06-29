8f141be110a87621a03f60e4c6ff7077e7dbd1f7
rldnrl <devpark1002@gmail.com>
2023-06-29 22:13:50 +0900

    feat: implement mobile nav

---
+++ src/components/mobile-nav.tsx
@@ -0,0 +1,91 @@
+"use client"
+
+import * as React from "react"
+import Link, { LinkProps } from "next/link"
+import { usePathname, useRouter } from "next/navigation"
+import { routes } from "@/constant/routes"
+import { siteMetadata } from "@/constant/site-metadata"
+
+import { cn } from "@/lib/utils"
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
+import { Button, buttonVariants } from "@/components/button"
+
+import { Icons } from "./icons"
+
+export const MobileNav = () => {
+  const pathname = usePathname()
+  const [open, setOpen] = React.useState(false)
+
+  return (
+    <Sheet open={open} onOpenChange={setOpen}>
+      <SheetTrigger asChild>
+        <Button
+          variant="ghost"
+          className="mr-2 px-0 text-base hover:bg-transparent focus-visible:bg-transparent focus-visible:ring-0 focus-visible:ring-offset-0 md:hidden"
+        >
+          <Icons.menu className="h-6 w-6" />
+          <span className="sr-only">Toggle Menu</span>
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="right">
+        <MobileLink
+          href="/"
+          className="flex items-center"
+          onOpenChange={setOpen}
+        >
+          <span className="font-bold">{siteMetadata.siteName}</span>
+        </MobileLink>
+        <div className="mt-6 flex flex-col gap-4">
+          {routes.map((route) => (
+            <div key={route.path} className="flex flex-col space-y-3">
+              <React.Fragment key={route.pathname}>
+                <MobileLink
+                  href={route.path}
+                  onOpenChange={setOpen}
+                  className={cn(
+                    buttonVariants({
+                      variant: route.path === pathname ? "secondary" : "ghost",
+                      size: "sm",
+                    }),
+                    "justify-start"
+                  )}
+                >
+                  {route.pathname}
+                </MobileLink>
+              </React.Fragment>
+            </div>
+          ))}
+        </div>
+      </SheetContent>
+    </Sheet>
+  )
+}
+
+interface MobileLinkProps extends LinkProps {
+  onOpenChange?: (open: boolean) => void
+  children: React.ReactNode
+  className?: string
+}
+
+function MobileLink({
+  href,
+  onOpenChange,
+  className,
+  children,
+  ...props
+}: MobileLinkProps) {
+  const router = useRouter()
+  return (
+    <Link
+      href={href}
+      onClick={() => {
+        router.push(href.toString())
+        onOpenChange?.(false)
+      }}
+      className={cn(className)}
+      {...props}
+    >
+      {children}
+    </Link>
+  )
+}
