fd38beeb4adca6980e422d691ece31696c625e6f
rldnrl <devpark1002@gmail.com>
2023-06-30 14:45:19 +0900

    feat: define github activity type

---
+++ src/types/github-activity.d.ts
@@ -0,0 +1,11 @@
+import { Activity } from "react-activity-calendar"
+
+export type Year = number | "last"
+
+export interface ApiResponse {
+  total: {
+    [year: number]: number
+    [year: string]: number // 'lastYear;
+  }
+  contributions: Array<Activity>
+}
