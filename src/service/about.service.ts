c20e1c33436f7c751070bb4b6e3b3914db81b90f
rldnrl <devpark1002@gmail.com>
2023-06-30 14:44:01 +0900

    feat: implement to allow mdx

--- src/service/about.service.ts
+++ src/service/about.service.ts
@@ -4,16 +4,24 @@ import rehypeHighlight from "rehype-highlight/lib"
 import rehypeSlug from "rehype-slug"
 
 import { About, Frontmatter } from "@/types/about"
+import { GithubCalendar } from "@/components/github-calendar"
 
 const ABOUT_DIR = "src/contents/about"
 
 export class AboutService {
   public static async fetchAbout(): Promise<About | null> {
     try {
-      const readFile = fs.readFileSync(ABOUT_DIR + `/about.md`, "utf-8")
+      const fileExtension = fs.existsSync(`${ABOUT_DIR}/about.mdx`)
+        ? ".mdx"
+        : ".md"
+      const readFile = fs.readFileSync(
+        `${ABOUT_DIR}/about${fileExtension}`,
+        "utf-8"
+      )
 
       const { frontmatter, content } = await compileMDX<Frontmatter>({
         source: readFile,
+        components: { GithubCalendar },
         options: {
           parseFrontmatter: true,
           mdxOptions: {
